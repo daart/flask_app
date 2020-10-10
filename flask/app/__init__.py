355c0837cb0f03fc4bcaf10c8738c18465cf96ea
Artem Kuzko <artem.kuzko@datarobot.com>
2020-10-10 14:17:10 +0300

    with mongo auth error

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = 'mongodb://daart:user1243@mongodb:27017/demo'
+app.config["MONGO_URI"] = 'mongodb://daart:pass1234@db:27017/flaskdb'
 mongo = PyMongo(app)
 
 CORS(app)
